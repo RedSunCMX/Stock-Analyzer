2a5b8619905c64b8dc4eae5af90542bda122f723
mardizzone <ardiz8@aol.com>
2013-06-13 11:01:05 -0700

    commit

---
+++ nicedatestring.py
@@ -0,0 +1,25 @@
+import datetime
+
+now = datetime.datetime.now()
+
+day = str(now.day)
+month = str(now.month)
+
+if len(day) == 1:
+	day = "0" + day
+	
+if len(month) == 1:
+	month = "0" + month
+
+def getnicedatetime():
+
+	if len(str(now.day)) == 1 and len(str(now.month)) == 1:
+		return str(now.year) + month + day
+		
+	if len(str(now.day)) == 2 and len(str(now.month)) == 1:
+		return str(now.year) + month + str(now.day)
+		
+	if len(str(now.day)) == 1 and len(str(now.month)) == 2:
+		return str(now.year) + str(now.month) + day
+
+print getnicedatetime()[:6]
